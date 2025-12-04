f900d19724bd0e01e1a00c6e3b60543630ee4f30
Chae Cramb <chae.cramb@digital.cabinet-office.gov.uk>
2025-12-04 16:06:49 +0000

    WIP

---
+++ tests/rag_answers/custom_deepeval/metrics/absence_of_factual_contradictions/test_absence_of_factual_contradictions_with_real_bedrock.py
@@ -0,0 +1,50 @@
+import pytest
+from deepeval.test_case import LLMTestCase
+
+from govuk_chat_evaluation.rag_answers.custom_deepeval.metrics.absence_of_factual_contradictions.absence_of_factual_contradictions import (
+    AbsenceOfFactualContradictions,
+)
+
+
+@pytest.mark.real_bedrock
+class TestAbsenceOfFactualContradictionsBedrockOpenAI:
+    """
+    Test the AbsenceOfFactualContradictions metric with real Bedrock API calls.
+    This test requires the Bedrock environment variable to be set with
+    `./scripts/export_aws_credentials.sh`.
+
+    It can be run with the command:
+    uv run pytest -m 'real_bedrock'
+    """
+
+    @pytest.mark.parametrize(
+        "llm_test_case, expected_score",
+        [
+            (
+                LLMTestCase(
+                    input="What is the capital of France?",
+                    actual_output="Paris is the capital of France.",
+                    expected_output="The capital of France is Paris.",
+                ),
+                1.0,
+            ),
+            (
+                LLMTestCase(
+                    input="What is the capital of France?",
+                    actual_output="London is the capital of France.",
+                    expected_output="The capital of France is Paris.",
+                ),
+                0.0,
+            ),
+        ],
+    )
+    @pytest.mark.asyncio
+    async def test_absence_of_factual_contradictions_score_bedrock(
+        self, bedrock_openai_judge, llm_test_case: LLMTestCase, expected_score: float
+    ):
+        metric = AbsenceOfFactualContradictions(
+            model=bedrock_openai_judge,
+            include_reason=False,
+        )
+        computed_score = await metric.a_measure(llm_test_case)
+        assert round(computed_score, 2) == pytest.approx(expected_score, rel=0.2)
