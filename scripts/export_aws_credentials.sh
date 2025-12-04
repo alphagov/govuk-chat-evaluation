a394db6b5ab541c1b35aed3ee6375ab9db097b01
Kevin Dew <kevindew@me.com>
2025-12-04 14:47:10 +0000

    Enhance AWS credentials export script
    This iterates the export_aws_credentials.sh primarily to prevent unnecessarily setting new credentials. It is a copy and paste from a different project I've been working on.
    
    It does a few things different to the previous script:
    
    1. It allows being called with an argument of the AWS role so ones other that govuk-test-developer can be used easily.
    2. If there is an existing .env.aws it checks the credentials in this using python-dotenv to call AWS (this was to avoid having to parse the .env.aws file and convert it into env vars for the script.
    3. If no .env.aws file exists or the credentials are expired new ones will be created
    
    Note, this script does have a risk that if credentials are just about to expire they could expire mid way through an evaluation run, a future iteration could be to check how long is left on the credentials (if an API does provide that functionality)

--- scripts/export_aws_credentials.sh
+++ scripts/export_aws_credentials.sh
@@ -1,16 +1,42 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "== Configuring AWS credentials =="
+aws_role="${1:-govuk-test-developer}"
 
-repo_root="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)"
+echo "== Configuring AWS credentials for role: $aws_role =="
+
+script_real_path="$(readlink -f "${BASH_SOURCE[0]}")"
+repo_root="$(cd "$(dirname "$script_real_path")"/.. && pwd)"
 config_path="${repo_root}/.env.aws"
 
-aws_credentials="$(gds aws govuk-test-developer -e --art 8h)"
+credentials_valid() {
+  local config_path="$1"
+  (
+    set -euo pipefail
+    uvx --from python-dotenv[cli] \
+        dotenv -f $config_path run --override \
+        -- \
+        aws sts get-caller-identity >/dev/null 2>&1
+  )
+}
+
+if [[ -f "$config_path" ]]; then
+  echo "Existing credentials found, checking validity"
+  if credentials_valid "$config_path"; then
+    echo "Existing AWS credentials are still valid. No update needed."
+    exit 0
+  else
+    echo "Existing credentials are invalid or expired. Fetching new credentials..."
+  fi
+else
+  echo "No existing credentials file. Fetching new credentials..."
+fi
+
+aws_credentials="$(gds aws "$aws_role" -e --art 8h)"
 relevant_credentials=$(echo "$aws_credentials" \
   | grep '^export ' \
   | sed -E 's/^export (.*);$/\1/')
 
 printf '%s\n' "$relevant_credentials" > "$config_path"
 
-echo "Written to $(basename "$config_path")"
+echo "Written to $config_path"
